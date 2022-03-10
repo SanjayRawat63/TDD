9b2312f2bc194cc1fe15a7ee66cf3718f65cae3c
Sanjay Rawat <anishrawatsanjay@gmail.com>
2022-03-10 11:34:21 +0530

    Fails For A Given Distance

--- CabService/src/test/java/co/Interleap/courses/tdd/StatementGeneratorTest.java
+++ CabService/src/test/java/co/Interleap/courses/tdd/StatementGeneratorTest.java
@@ -9,7 +9,12 @@ class StatementGeneratorTest {
 	@Test
 	void shouldCalculateCostForAGivenTime() {
 		assertEquals(1,new StatementGenerator().TotalCost(0,1));
-	}	
+	}
+	
+	@Test
+	void shouldCalculateCostForAGivenDistance() {
+		assertEquals(10,new StatementGenerator().TotalCost(10,0));
+	}
 	
 
 }
