7b14835c1477387648d16bea5e707fdb65979166
Sanjay Rawat <anishrawatsanjay@gmail.com>
2022-03-10 12:48:04 +0530

    Code After Extract named constants

--- CabService/src/main/java/co/inteleap/course/StatementGenerator.java
+++ CabService/src/main/java/co/inteleap/course/StatementGenerator.java
@@ -1,9 +1,12 @@
 package co.inteleap.course;
 
 public class StatementGenerator {
+	public static final int FAIR_PER_KM=10;
+	public static final int FAIR_PER_MINUTE=1;
+	
 	public double TotalFare(double Distance,double Time)
 	{
-		return 10*Distance + 1*Time;
+		return FAIR_PER_KM * Distance + FAIR_PER_MINUTE * Time;
 	}
 
 }
