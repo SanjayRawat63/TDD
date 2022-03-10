726c5db3a1d6ecd8664d1400e43cb3e34ea1d0ea
Sanjay Rawat <anishrawatsanjay@gmail.com>
2022-03-10 12:17:11 +0530

    Cab Service Example

---
+++ CabService/src/test/java/co/inteleap/course/StatementGenereatorTest.java
@@ -0,0 +1,14 @@
+package co.inteleap.course;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StatementGenereatorTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
