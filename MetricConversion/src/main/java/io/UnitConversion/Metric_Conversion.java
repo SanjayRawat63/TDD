525dc6a7b3e949134d5b774b2903eecb70007691
Sanjay Rawat <anishrawatsanjay@gmail.com>
2022-03-13 12:56:22 +0530

    Metric Conversion

--- MetricConversion/src/main/java/io/UnitConversion/Metric_Conversion.java
+++ MetricConversion/src/main/java/io/UnitConversion/Metric_Conversion.java
@@ -2,28 +2,28 @@ package io.UnitConversion;
 
 public class Metric_Conversion {
 	
-	public double ParseString(String str)
+	public static final double INCH_TO_M_FACTOR=0.0254;
+	public static final double KM_TO_M_FACTOR=1000;
+	
+	public double INCH_TO_M(double value)
 	{
-		str=str.replaceAll("[a-zA-Z\\s]","");
-		return (str==null||str.isEmpty()) ? 0 : Double.parseDouble(str);
-	}
-	public double km_to_m(double value)
-	{
-		return (value*1000);
-	}
-	public double M_to_Km(double value)
-	{
-		return value/1000;
-	}
-	public double M_to_Cm(double value)
-	{
-		return value*100;
-	}
-	public double Cm_to_M(double value)
-	{
-		return value/100;
+		return  INCH_TO_M_FACTOR * value;
 	}
 	
+	public double M_TO_INCH(double value)
+	{
+		return value/INCH_TO_M_FACTOR;
+	}
+	
+	public double KM_TO_M(double value)
+	{
+		return  KM_TO_M_FACTOR * value;
+	}
+	
+	public double M_TO_KM(double value)
+	{
+		return value/KM_TO_M_FACTOR;
+	}
 	
 
 }
