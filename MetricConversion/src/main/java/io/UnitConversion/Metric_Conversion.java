ea02f9ef9f9a79c1bd16b332b7eb99b1236151be
Sanjay Rawat <anishrawatsanjay@gmail.com>
2022-03-13 18:55:46 +0530

    Metric conversion file

--- MetricConversion/src/main/java/io/UnitConversion/Metric_Conversion.java
+++ MetricConversion/src/main/java/io/UnitConversion/Metric_Conversion.java
@@ -2,43 +2,10 @@ package io.UnitConversion;
 
 public class Metric_Conversion {
 	
-	public static final double INCH_TO_M_FACTOR=0.0254;
-	public static final double KM_TO_M_FACTOR=1000;
 	
 	public double Unit_Converter(UnitConverter conv)
 	{
-	    String from_unit=conv.getFromUnit();
-	    String to_unit=conv.getToUnit();
-	    double value_to_be_converted= conv.getValue();
-	    if(from_unit.equals("in")&&to_unit.equals("m"))
-		   return INCH_TO_M(value_to_be_converted);
-	    else if(from_unit.equals("m")&&to_unit.equals("in"))
-			   return M_TO_INCH(value_to_be_converted);
-	    if(from_unit.equals("km")&&to_unit.equals("m"))
-			   return KM_TO_M(value_to_be_converted);
-	    else	
-	       return M_TO_KM(value_to_be_converted);
-	 
+		return conv.converter();
 	}
-	public double INCH_TO_M(double value)
-	{
-		return  INCH_TO_M_FACTOR * value;
-	}
-	
-	public double M_TO_INCH(double value)
-	{
-		return value/INCH_TO_M_FACTOR;
-	}
-	
-	public double KM_TO_M(double value)
-	{
-		return  KM_TO_M_FACTOR * value;
-	}
-	
-	public double M_TO_KM(double value)
-	{
-		return value/KM_TO_M_FACTOR;
-	}
-	
 
 }
