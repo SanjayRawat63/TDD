0c811390f796c65e13b22960e58eefe55a0e568f
Sanjay Rawat <anishrawatsanjay@gmail.com>
2022-03-13 19:31:19 +0530

    Unit Converter Improved

--- MetricConversion/src/main/java/io/UnitConversion/UnitConverter.java
+++ MetricConversion/src/main/java/io/UnitConversion/UnitConverter.java
@@ -9,68 +9,57 @@ public class UnitConverter {
 	 static double CENTIMETERS_TO_M_FACTOR = 0.01;
 	 static double METERS_TO_M_FACTOR = 1;
 	 static double KILOMETERS_TO_M_FACTOR =1000;
-	 private double meters ,converted;
+	 private static double CONVERSION_FACTOR;
 	 
 	 public UnitConverter(String from, String to, double value)
 		{
 			this.FROM_UNIT=from;
 			this.TO_UNIT=to;
 			this.value=value;
-		}	
+		}
+	 
+	 public  double Conversion_Factor(String unit)
+		 {
+			 if(unit.equals("in"))
+			  {
+			   CONVERSION_FACTOR=INCHES_TO_M_FACTOR;
+			  }
+			  else if(unit.equals("mm"))
+			  {
+			   CONVERSION_FACTOR = MILLIMETERS_TO_M_FACTOR;
+			  }
+			  else if(unit.equals("cm"))
+			  {
+			   CONVERSION_FACTOR =  CENTIMETERS_TO_M_FACTOR;
+			  }
+			  else if(unit.equals("m"))
+			  {
+			   CONVERSION_FACTOR = METERS_TO_M_FACTOR;
+			  }
+			  else if(unit.equals("km"))
+			  {
+			  CONVERSION_FACTOR = KILOMETERS_TO_M_FACTOR;
+			  }
+			 return CONVERSION_FACTOR;
+		 }
+	 
 	 public double converter()
 	 {
 		 double converted_to_meter=GivenUnitToMeters(value);
 		 double converted_value=MeterToGivenConversionUnit(converted_to_meter);
 		 return converted_value;
 	 }
-	 public double GivenUnitToMeters(double val) 
+	 
+	 public double GivenUnitToMeters(double value) 
 	 {
-	  if(FROM_UNIT.equals("in"))
-	  {
-	   meters = (val*INCHES_TO_M_FACTOR);
-	  }
-	  else if(FROM_UNIT.equals("mm"))
-	  {
-	   meters = (val*MILLIMETERS_TO_M_FACTOR);
-	  }
-	  else if(FROM_UNIT.equals("cm"))
-	  {
-	   meters = (val * CENTIMETERS_TO_M_FACTOR);
-	  }
-	  else if(FROM_UNIT.equals("m"))
-	  {
-	   meters = (val* METERS_TO_M_FACTOR);
-	  }
-	  else if(FROM_UNIT.equals("km"))
-	  {
-	   meters = (val*KILOMETERS_TO_M_FACTOR);
-	  }
-	  return meters;
+		double converter_factor=Conversion_Factor(FROM_UNIT);
+	    return value*converter_factor;
 	 }
 	 
 	 public double MeterToGivenConversionUnit(double meters) 
 	 {
-	  if(TO_UNIT.equals("in"))
-	  {
-	   converted = (meters/INCHES_TO_M_FACTOR);
-	  }
-	  else if(TO_UNIT.equals("mm")) 
-	  {
-	   converted = (meters/MILLIMETERS_TO_M_FACTOR);
-	  }
-	  else if(TO_UNIT.equals("cm")) 
-	  {
-	   converted = (meters/CENTIMETERS_TO_M_FACTOR);
-	  }
-	  else if(TO_UNIT.equals("m")) 
-	  {
-	   converted =(meters/METERS_TO_M_FACTOR);
-	  }
-	  else if(TO_UNIT.equals("km")) 
-	  {
-	   converted = (meters/KILOMETERS_TO_M_FACTOR);
-	  }
-	  return converted;
+		 double converter_factor=Conversion_Factor(TO_UNIT);
+		 return meters/converter_factor;
 	 }	
 	}
 	
