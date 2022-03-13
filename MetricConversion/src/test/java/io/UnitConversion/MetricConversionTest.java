a30af50e3b51c8b0d0798df17973fa693dc8f544
Sanjay Rawat <anishrawatsanjay@gmail.com>
2022-03-13 18:57:31 +0530

    Metric Conversion Test

--- MetricConversion/src/test/java/io/UnitConversion/MetricConversionTest.java
+++ MetricConversion/src/test/java/io/UnitConversion/MetricConversionTest.java
@@ -11,10 +11,11 @@ class MetricConversionTest {
 	public void ShouldConvertINCHToM()
 	{
 		Metric_Conversion metric_conversion= new Metric_Conversion();
-		UnitConverter unitconverter=new UnitConverter("in","m",1);
+		UnitConverter unitconverter=new UnitConverter("in","m",2);
 		double converted_value=metric_conversion.Unit_Converter(unitconverter);
-		assertEquals(0.0254,converted_value);
+		assertEquals(0.0508,converted_value);
 	}
+	
 	@Test
 	public void ShouldConvertMToINCH()
 	{
@@ -32,6 +33,7 @@ class MetricConversionTest {
 		double converted_value=metric_conversion.Unit_Converter(unitconverter);
 		assertEquals(1000,converted_value);
 	}
+	
 	@Test
 	public void ShouldConvertMToKM()
 	{
@@ -39,5 +41,41 @@ class MetricConversionTest {
 		UnitConverter unitconverter=new UnitConverter("m","km",1000);
 		double converted_value=metric_conversion.Unit_Converter(unitconverter);
 		assertEquals(1,converted_value);
+	}
+	
+	@Test
+	public void ShouldConvertMToCM()
+	{
+		Metric_Conversion metric_conversion= new Metric_Conversion();
+		UnitConverter unitconverter=new UnitConverter("m","cm",1);
+		double converted_value=metric_conversion.Unit_Converter(unitconverter);
+		assertEquals(100,converted_value);
+	}
+	
+	@Test
+	public void ShouldConvertCMToM()
+	{
+		Metric_Conversion metric_conversion= new Metric_Conversion();
+		UnitConverter unitconverter=new UnitConverter("cm","m",100);
+		double converted_value=metric_conversion.Unit_Converter(unitconverter);
+		assertEquals(1,converted_value);
+	}
+	
+	@Test
+	public void ShouldConvertINCHToKM()
+	{
+		Metric_Conversion metric_conversion= new Metric_Conversion();
+		UnitConverter unitconverter=new UnitConverter("in","km",10);
+		double converted_value=metric_conversion.Unit_Converter(unitconverter);
+		assertEquals(0.000254,converted_value);
+	}
+	
+	@Test
+	public void ShouldConvertKMToINCH()
+	{
+		Metric_Conversion metric_conversion= new Metric_Conversion();
+		UnitConverter unitconverter=new UnitConverter("km","in",0.000254);
+		double converted_value=metric_conversion.Unit_Converter(unitconverter);
+		assertEquals(10,converted_value);
 	}
 }
