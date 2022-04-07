801fe2d0766426bf9bd3c19b0efb29a0f563129a
Sanjay Rawat <63999697+SanjayRawat63@users.noreply.github.com>
2022-04-07 18:38:05 +0530

    convert to functional programmin

--- CabService/src/main/java/co/inteleap/course/Ride.java
+++ CabService/src/main/java/co/inteleap/course/Ride.java
@@ -1,21 +1,21 @@
 package co.inteleap.course;
 
+import java.util.function.Supplier;
+
 public class Ride {
 
-	private double Distance;
-	private double Time;
-	public static final int FARE_PER_KM=10;
-	public static final int FARE_PER_MINUTE=1;
-	
-	public Ride(double distance,double time)
-	{
-		this.Distance=distance;
-		this.Time=time;
-	}
-	
-	public double getTotalFare()
-	{
-		return FARE_PER_KM * Distance + FARE_PER_MINUTE* Time;
-	}
-	
+    private double Distance;
+    private double Time;
+    public static final int FARE_PER_KM = 10;
+    public static final int FARE_PER_MINUTE = 1;
+
+    public Ride(double distance, double time) {
+        this.Distance = distance;
+        this.Time = time;
+    }
+
+    public Supplier<Double> getTotalFare =
+            () -> FARE_PER_KM * Distance + FARE_PER_MINUTE * Time;
+
+
 }
--- CabService/src/main/java/co/inteleap/course/Statement.java
+++ CabService/src/main/java/co/inteleap/course/Statement.java
@@ -1,34 +1,35 @@
 package co.inteleap.course;
 
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.function.ToIntFunction;
+import java.util.stream.Collectors;
 
-public class Statement{
-        List<Ride> rides;
-		
-        public Statement(List rides)
-		{
-			this.rides=rides;
-		}
-		
-		public double getTotalFare()
-		{
-			double totalfare=0;
-			for(Ride ride:rides)
-			    totalfare+= ride.getTotalFare();
-			
-		   return totalfare;
-		}
-		
-		public int getNumberOfRides()
-		{
-			return rides.size();
-		}
-		
-		public double getAvgFarePerRide()
-		{
-			return getTotalFare()/getNumberOfRides();
-		}
-		
-		
-	}
+public class Statement {
+
+    List<Ride> rides;
+
+    public Statement(List rides) {
+        this.rides = rides;
+    }
+
+    public double getTotalFare()
+    {
+        double totalfare=0;
+        for(Ride ride:rides)
+            totalfare+= ride.getTotalFare.get();
+
+        return totalfare;
+    }
+
+
+    public Supplier<Integer> getNumberOfRides =
+            () -> rides.size();
+
+    public Supplier<Double> getAvgFarePerRide =
+            () -> getTotalFare() / getNumberOfRides.get();
+
+
+}
 
