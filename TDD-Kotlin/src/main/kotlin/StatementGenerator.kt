7a98d6eaa0e21fcadfcae3e6689542235ba7f4bb
Sanjay Rawat <63999697+SanjayRawat63@users.noreply.github.com>
2022-04-08 12:07:49 +0530

    After Extract named constants

--- TDD-Kotlin/src/main/kotlin/StatementGenerator.kt
+++ TDD-Kotlin/src/main/kotlin/StatementGenerator.kt
@@ -1,5 +1,7 @@
 class StatementGenerator {
+    val FARE_PER_KM=10
+    val FARE_PER_MINUTE=1
     fun totalFare(time: Double, distance: Double): Double {
-        return time * 1 + distance * 10
+        return time * FARE_PER_MINUTE + distance * FARE_PER_KM
     }
 }
