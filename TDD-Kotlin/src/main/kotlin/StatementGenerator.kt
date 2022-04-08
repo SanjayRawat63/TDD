8b153a5e9c8e3e4d77789cd3197df01d63113f5e
Sanjay Rawat <63999697+SanjayRawat63@users.noreply.github.com>
2022-04-08 13:25:36 +0530

    Calculation totalfare avgfareperride noofRide

--- TDD-Kotlin/src/main/kotlin/StatementGenerator.kt
+++ TDD-Kotlin/src/main/kotlin/StatementGenerator.kt
@@ -1,9 +1,19 @@
 class StatementGenerator {
-
-    fun totalFare(rides: List<Ride>): Double {
+    var rides:List<Ride>
+    constructor(Rides: List<Ride>)
+    {
+        rides=Rides
+    }
+    fun totalFare(): Double {
         var totalfare: Double = 0.0
         for (ride in rides)
             totalfare += ride.Fare()
         return totalfare
+    }
+    fun getNoOfRides():Int{
+        return rides.size
+    }
+    fun getAvgFarePerRide():Double{
+        return totalFare()/getNoOfRides()
     }
 }
--- TDD-Kotlin/src/test/kotlin/StatementGeneratorTest.kt
+++ TDD-Kotlin/src/test/kotlin/StatementGeneratorTest.kt
@@ -4,8 +4,10 @@ import kotlin.test.assertEquals
 class StatementGeneratorTest {
     @Test
     fun shouldCalcualteFareForMultipleRide(){
-        val statementGenerator=StatementGenerator()
         val listOfRide= listOf(Ride(1.0,1.0),Ride(2.0,10.0))
-        assertEquals(113.0,statementGenerator.totalFare(listOfRide))
+        val statementGenerator=StatementGenerator(listOfRide)
+        assertEquals(113.0,statementGenerator.totalFare())
+        assertEquals(2,statementGenerator.getNoOfRides())
+        assertEquals(56.5,statementGenerator.getAvgFarePerRide())
     }
 }
